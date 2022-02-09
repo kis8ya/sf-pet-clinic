2a9ff0a699eba3feadd5eb00ed70b10aa405a222
kis8ya <admin.kisbox@gmail.com>
2022-02-09 21:59:53 +0300

    initial spring initializr files

---
+++ src/main/java/com/github/kis8ya/sfpetclinic/SfPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.github.kis8ya.sfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfPetClinicApplication.class, args);
+    }
+
+}
